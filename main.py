ca51c1b46545fa17475470aae1bad75fc78ed00a
Anna Ropai <ropai.anna@gmail.com>
2022-01-17 23:59:16 +0300

    Make interface for choosing how to play

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ from Field import Field
 from tkinter import *
 from typing import Union
 from alpha_beta import make_alpha_beta
-from alpha_beta import INF
-import time
 
 CANVAS_WIDTH = 1200
 CANVAS_HEIGHT = 650
 
 root = Tk()
+root.geometry("1200x650")
 c = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg='white')
-text_label = Label(text="Player 1, please type the number of the column\nwhere you'd lke to put your chip:",
-                   justify=LEFT,
-                   bg="#BDDCFF",
-                   borderwidth=2, relief="solid")
-entry_col = Entry(width=10, borderwidth=2, relief="solid")
+text_label = Label(c)
+entry_col = Entry(width=10, borderwidth=1, relief="solid")
+button_no_bot = Button()
+button_with_bot = Button()
 
 field = Field()
 somebody_wins = False
 moves = 0
 player = False
 print_field = True
+is_bot_here = False
 
 
 def try_turn(col: Union[int, str]) -> int:
@@ -36,7 +35,8 @@ def try_turn(col: Union[int, str]) -> int:
         text_label['text'] = "Please, type the correct number of the column"
         return 2
     somebody_wins = turn_result
-    # player ^= 1
+    if not is_bot_here:
+        player ^= 1
     moves += 1
     return somebody_wins
 
@@ -44,6 +44,10 @@ def try_turn(col: Union[int, str]) -> int:
 def make_turn(col: Union[int, str]) -> None:
     result = try_turn(col)
     if result != 2:
+        if not is_bot_here:
+            text_label[
+                'text'] = "Player {0}, please type the number of the column\nwhere you'd lke to put your chip:".format(
+                player + 1)
         col = int(col) - 1
         row = 6 - field.used[col]
         if player == 1:
@@ -51,7 +55,10 @@ def make_turn(col: Union[int, str]) -> None:
         else:
             colour = "red"
         cell = c.create_oval(col * 100 + 10, row * 100 + 10, col * 100 + 90, row * 100 + 90, fill=colour, width=3)
-        c.tag_bind(cell, '<ButtonPress-1>', on_click_cell)
+        if is_bot_here:
+            c.tag_bind(cell, '<ButtonPress-1>', on_click_cell_with_bot)
+        else:
+            c.tag_bind(cell, '<ButtonPress-1>', on_click_cell_no_bot)
         c.update()
         print(field)
         if somebody_wins:
@@ -82,7 +89,7 @@ def make_bot_turn() -> None:
     row = 6 - field.used[col]
     colour = "green"
     cell = c.create_oval(col * 100 + 10, row * 100 + 10, col * 100 + 90, row * 100 + 90, fill=colour, width=3)
-    c.tag_bind(cell, '<ButtonPress-1>', on_click_cell)
+    c.tag_bind(cell, '<ButtonPress-1>', on_click_cell_with_bot)
     c.update()
     print(field)
     if field.check_win(True):
@@ -97,65 +104,107 @@ def make_bot_turn() -> None:
         text_label['text'] = "Draw!"
 
 
-def on_change(entry_col) -> None:
+def on_change_with_bot(entry_col) -> None:
     if somebody_wins or moves >= 42:
         entry_col.widget.delete(0, 'end')
         return
     col = entry_col.widget.get()
     entry_col.widget.delete(0, 'end')
     make_turn(col)
-    time.sleep(0.5)
     make_bot_turn()
 
 
-def on_click_cell(event) -> None:
+def on_change_no_bot(entry_col) -> None:
+    if somebody_wins or moves >= 42:
+        entry_col.widget.delete(0, 'end')
+        return
+    col = entry_col.widget.get()
+    entry_col.widget.delete(0, 'end')
+    make_turn(col)
+
+
+def on_click_cell_with_bot(event) -> None:
     print("moves: ", moves)
     print("-" * 40)
     if somebody_wins or moves >= 42:
         return
     col = event.x // 100 + 1
     make_turn(col)
-    time.sleep(0.5)
     make_bot_turn()
 
 
-def main_no_bot():
-    root.resizable(width=False, height=False)
+def on_click_cell_no_bot(event) -> None:
+    print("moves: ", moves)
+    print("-" * 40)
+    if somebody_wins or moves >= 42:
+        return
+    col = event.x // 100 + 1
+    make_turn(col)
+
+
+def start_mains():
+    c['width'] = CANVAS_WIDTH
+    c['height'] = CANVAS_HEIGHT
+    entry_col.pack()
+    global text_label
+    text_label.config(text="Player 1, please type the number of the column\nwhere you'd lke to put your chip:",
+                      font=("Courier", 12), justify=LEFT)
+    # text_label['bg'] = "#BDDCFF"
+    # text_label['borderwidth'] = 2
+    # text_label['relief'] = "solid"
+    button_no_bot.destroy()
+    button_with_bot.destroy()
     c.create_rectangle(0, 0, 700, 600, fill='#8D84FF')
-    for col in range(7):
-        for row in range(6):
-            cell = c.create_oval(col * 100 + 10, row * 100 + 10, col * 100 + 90, row * 100 + 90, fill="white", width=3)
-            c.tag_bind(cell, '<ButtonPress-1>', on_click_cell)
+    text_label.place(x=710, y=10)
+    entry_col.place(x=1050, y=27)
     for col in range(7):
         number_frame = Frame(root, width=100, height=50, bg="white")
         number_frame.pack_propagate(False)
         number_frame.place(x=col * 100, y=601)
         number = Label(number_frame, text=str(col + 1), bg='white', fg='black', font='Arial, 32')
         number.place(relx=0.5, rely=0.5, anchor=CENTER)
-    text_label.place(x=710, y=10)
-    entry_col.place(x=1050, y=27)
-    entry_col.bind("<Return>", on_change)
+
+
+def main_no_bot():
+    start_mains()
+    for col in range(7):
+        for row in range(6):
+            cell = c.create_oval(col * 100 + 10, row * 100 + 10, col * 100 + 90, row * 100 + 90, fill="white", width=3)
+            c.tag_bind(cell, '<ButtonPress-1>', on_click_cell_no_bot)
+    entry_col.bind("<Return>", on_change_no_bot)
     c.pack()
-    root.mainloop()
+
+
+def main_with_bot():
+    global is_bot_here
+    is_bot_here = True
+    start_mains()
+    for col in range(7):
+        for row in range(6):
+            cell = c.create_oval(col * 100 + 10, row * 100 + 10, col * 100 + 90, row * 100 + 90, fill="white", width=3)
+            c.tag_bind(cell, '<ButtonPress-1>', on_click_cell_with_bot)
+    entry_col.bind("<Return>", on_change_with_bot)
+    c.pack()
 
 
 def main():
     root.resizable(width=False, height=False)
-    c.create_rectangle(0, 0, 700, 600, fill='#8D84FF')
-    for col in range(7):
-        for row in range(6):
-            cell = c.create_oval(col * 100 + 10, row * 100 + 10, col * 100 + 90, row * 100 + 90, fill="white", width=3)
-            c.tag_bind(cell, '<ButtonPress-1>', on_click_cell)
-    for col in range(7):
-        number_frame = Frame(root, width=100, height=50, bg="white")
-        number_frame.pack_propagate(False)
-        number_frame.place(x=col * 100, y=601)
-        number = Label(number_frame, text=str(col + 1), bg='white', fg='black', font='Arial, 32')
-        number.place(relx=0.5, rely=0.5, anchor=CENTER)
-    text_label.place(x=710, y=10)
-    entry_col.place(x=1050, y=27)
-    entry_col.bind("<Return>", on_change)
+    global text_label
+    text_label.config(text="Choose how you'd like to play:", justify=CENTER, bg="#BDDCFF", borderwidth=2,
+                      relief="solid", font=("Courier", 40))
+    text_label.place(x=500, y=20)
+    text_label.pack()
+    entry_col.pack_forget()
+    button_with_bot.config(text="With bot", command=main_with_bot, font=("Courier", 44), bg="#E48A7F",
+                           activebackground="#A1FF89")
+    button_no_bot.config(text="With another player", command=main_no_bot, font=("Courier", 44), bg="#CD7FE4",
+                         activebackground="#A1FF89")
+    # button_no_bot.place(x=200, y=100)
+    # button_with_bot.place(x=800, y=100)
+    # c.update()
     c.pack()
+    button_no_bot.pack()
+    button_with_bot.pack()
     root.mainloop()
 
 
